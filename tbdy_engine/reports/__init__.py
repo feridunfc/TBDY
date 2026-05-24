a5f8802de3dce4fd7e7567731ddd2a06a91810ec
feridunfc <yalduz@gmail.com>
2026-05-24 11:45:42 +0300

    Sprint 2E: export reporting facade

--- tbdy_engine/reports/__init__.py
+++ tbdy_engine/reports/__init__.py
@@ -1,5 +1,12 @@
 from __future__ import annotations
 
+from .facade import ReportingFacade, ReportingResult
 from .report_plan import PlannedReport, ReportPlan, ReportPlanner
 
-__all__ = ["PlannedReport", "ReportPlan", "ReportPlanner"]
+__all__ = [
+    "PlannedReport",
+    "ReportPlan",
+    "ReportPlanner",
+    "ReportingFacade",
+    "ReportingResult",
+]
