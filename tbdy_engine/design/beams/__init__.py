ec444c3ff022b8ad194978d4d648bd08556c78c4
feridunfc <yalduz@gmail.com>
2026-05-30 15:05:31 +0300

    Export beam core context and geometry calculator

--- tbdy_engine/design/beams/__init__.py
+++ tbdy_engine/design/beams/__init__.py
@@ -1,11 +1,22 @@
+from tbdy_engine.design.beams.context import BeamModelContext, validate_beam_model_context
 from tbdy_engine.design.beams.evaluation_package import (
     BeamCheckEvaluation,
     BeamDesignModule,
     BeamEvaluationPackage,
     build_beam_evaluation_packages,
 )
+from tbdy_engine.design.beams.calculators import (
+    GeometryCheck,
+    GeometryResult,
+    TBDYGeometryCalculator,
+)
 
 __all__ = [
+    "BeamModelContext",
+    "validate_beam_model_context",
+    "GeometryCheck",
+    "GeometryResult",
+    "TBDYGeometryCalculator",
     "BeamCheckEvaluation",
     "BeamDesignModule",
     "BeamEvaluationPackage",
