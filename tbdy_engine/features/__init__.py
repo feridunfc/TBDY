c283cb477a73ac35dcec0b7adc85475666cd6719
feridunfc <yalduz@gmail.com>
2026-06-17 11:33:15 +0300

    C13.3-P3 harden no-live artifact contract

--- tbdy_engine/features/__init__.py
+++ tbdy_engine/features/__init__.py
@@ -1,6 +1,14 @@
 """FeatureSnapshot foundation exports for C4/C13 proof layers."""
+from tbdy_engine.features.check_preflight_diagnostics import build_check_preflight_diagnostic_report
 from tbdy_engine.features.diagnostics import FeatureDiagnostic, FeatureDiagnosticCode, FeatureDiagnosticSeverity
 from tbdy_engine.features.evidence import FeatureEvidence, FeatureEvidenceStatus
+from tbdy_engine.features.feature_snapshot_artifact_validator import (
+    scan_for_forbidden_engineering_verdicts,
+    validate_artifact_file_set,
+    validate_check_preflight_diagnostic_report,
+    validate_feature_snapshot_artifact_manifest,
+    validate_feature_snapshot_report_payload,
+)
 from tbdy_engine.features.feature_snapshot_artifacts import (
     build_feature_snapshot_artifact_manifest,
     build_feature_snapshot_report_payload,
@@ -32,11 +40,17 @@ __all__ = [
     "UnitNormalization",
     "build_c13_3_p0_feature_snapshot",
     "build_c13_3_p1_feature_snapshot",
+    "build_check_preflight_diagnostic_report",
     "build_feature_snapshot_artifact_manifest",
     "build_feature_snapshot_from_source_rows",
     "build_feature_snapshot_report_payload",
     "normalize_value",
     "render_feature_snapshot_html_report",
     "render_feature_snapshot_markdown_report",
+    "scan_for_forbidden_engineering_verdicts",
     "source_family_projection_report",
+    "validate_artifact_file_set",
+    "validate_check_preflight_diagnostic_report",
+    "validate_feature_snapshot_artifact_manifest",
+    "validate_feature_snapshot_report_payload",
 ]
