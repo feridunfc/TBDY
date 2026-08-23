85ab60f2ddd5466dafd48285c61cef283a387b85
feridunfc <yalduz@gmail.com>
2026-08-23 10:09:33 +0300

    VS-4A add reviewed source metadata package

---
+++ tbdy_engine/regulatory/sources/__init__.py
@@ -0,0 +1,5 @@
+"""Reviewed regulatory-source metadata packages; no regulation text is stored here."""
+
+from .tbdy2018 import build_vs4a_authority_catalog
+
+__all__ = ["build_vs4a_authority_catalog"]
